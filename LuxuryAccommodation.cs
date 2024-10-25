741716e25fe7605ec9deddc07bf0af308dcf2986
Võ Minh Khang <minhkhangvo.ag@gmail.com>
2024-10-25 12:53:57 +0700

    Update LuxuryAccommodation.cs

--- LuxuryAccommodation.cs
+++ LuxuryAccommodation.cs
@@ -1,4 +1,4 @@
-﻿using System;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -26,5 +26,6 @@ namespace Management_Module
             this.maxPerson = maxPerson;
             this.cost1NightLuxury = cost1NightLuxury;
         }
+        public int getMaxPerson () { return maxPerson; }    
     }
 }
